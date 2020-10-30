e6025068b37b929ba644e9f8496ad0357e6770f5
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-10-29 22:41:27 -0400

    Add additional solutions

--- reverseStr.js
+++ reverseStr.js
@@ -15,3 +15,36 @@ return reversedStr
 }
 
 reverse("jerlissa")
+
+// First Algorithm Udemy Course Solution
+
+function reverseOne(str){
+    return arr = str.split('').reverse().join('')
+  }
+  
+  reverseOne("hello")
+  
+  // Second Algorithm Udemy Course Solution
+  
+  function reverseTwo(str){
+    let reversed = "";
+  
+    for (let char of str) {
+      // reversed will be equal to the character that is now added on to the empty string 'reversed'
+  
+      reversed = char + reversed;
+    }
+    return reversed
+  }
+  
+  reverseTwo("hola")
+  
+  // Third Algorithm Udemy Course Solution
+  
+  function reverseThree(str){
+    return str.split('').reduce((reversed, char) => {
+      return char + reversed
+    })
+  }
+  
+  reverseThree("hi there")
